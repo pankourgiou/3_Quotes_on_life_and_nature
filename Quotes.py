2f2b481a7950af9ab3ee16ee31508281c9b4c912
L <60981745+pankourgiou@users.noreply.github.com>
2025-04-06 20:49:37 +0300

    Create Quotes.py

---
+++ Quotes.py
@@ -0,0 +1,4 @@
+#Those quotes are simple but true ideas deriving from life.
+print("Mountains do not speak, yet they teach us permanence.")
+print("To exist is already to be extraordinary.")
+print("Nature does not perform. It just is. So can you.")
